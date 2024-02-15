eada59c2a0c33659434db2e4d02481b91d5e523c
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-02-15 16:36:46 +0530

    Create header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Pages
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Dashboard from "./pages/Dashboard";
@@ -8,9 +10,13 @@ import Projects from "./pages/Projects";
 import SignIn from "./pages/Signin";
 import SignUp from "./pages/Signup";
 
+//Components
+import Header from "./components/Header";
+
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
