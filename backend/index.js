332d9741ced318542afaca8b8a008b15274da20c
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-02-15 22:39:34 +0530

    create user model route and controller

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import dbConnect from "./configs/dbConfig.js";
+import UserRoutes from "./routes/user.routes.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 6001;
+
+//User Routes
+app.use("/user", UserRoutes);
 
 app.use((req, res, next) => {
   console.log(`${req.method} =====> URL: ${req.url}`);
